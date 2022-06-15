cf742b6130f24eafc76afab4dd74837684db8dcd
Elvey <matt.elvey@blueflag.com.au>
2022-06-15 14:48:59 +0930

    feat: limit max size of batch put

--- packages/92green-rxjs/src/eventbus/batchPut.js
+++ packages/92green-rxjs/src/eventbus/batchPut.js
@@ -1,8 +1,9 @@
 // @flow
 // Deprecated use 92green-aws-rxjs
 import {Observable, interval, zip, of} from "rxjs";
-import {map, filter, expand, bufferCount, concatMap,share, throttle} from 'rxjs/operators';
+import {map, filter, expand, endWith, scan, concatMap,share, throttle} from 'rxjs/operators';
 const MAX_EVENT_BRIDGE_PUT = 10;
+const MAX_SIZE = 256000;
 const DEFAULT_THROTTLE_MS = 500;
 type Config = {
     eventBridge: any,
@@ -18,13 +19,45 @@ const RETRY_ON = [
     "InternalFailure"
 ];
 
+let END = Symbol('THE_END');
+
+const getSize = (incoming) => Buffer.byteLength(Buffer.from(JSON.stringify(incoming)));
+
 export default (config: Config) => (obs: Observable) => {
     const sendToEventBus = (obs) => {
         let input  = obs.pipe(share());
         let results = obs.pipe(
+            endWith([END]),
             map(([record]) => record),
-            bufferCount(MAX_EVENT_BRIDGE_PUT),
-            concatMap(records => config.eventBridge.putEvents({Entries: records}).promise()),
+            scan((recordBuffer, incoming) => {
+                if(incoming === END) {
+                    return {...recordBuffer, remainder: END};
+                }
+                let {remainder, records, size} = recordBuffer;
+                // TODO: switch size calculation to https://github.com/miktam/sizeof/blob/master/index.js
+                if(recordBuffer.remainder) {
+                    const sizeOfRemainder = getSize(remainder);
+                    if(sizeOfRemainder > MAX_SIZE) {
+                        throw new Error('Incoming event is too big for event bridge');
+                    }
+                    records = [remainder];
+                    size = sizeOfRemainder;
+                }
+
+                const incomingSize = getSize(incoming);
+                if(incomingSize + size > MAX_SIZE || records.length >= MAX_EVENT_BRIDGE_PUT) {
+                    return {records, size, remainder: incoming};
+                }
+
+                return {
+                    records: [...records, incoming],
+                    size: size + incomingSize,
+                    remainder: undefined
+                };
+            }, {records: [], remainder: undefined, size: 0}),
+            filter(({remainder}) => !!remainder),
+            //bufferCount(MAX_EVENT_BRIDGE_PUT),
+            concatMap(({records}) => config.eventBridge.putEvents({Entries: records}).promise()),
             concatMap(ii => ii.Entries)
         );
         return zip(
@@ -34,7 +67,7 @@ export default (config: Config) => (obs: Observable) => {
             map(([input, result]) => {
                 let [record, info] = input;
                 return [record, {
-                    ...info, 
+                    ...info,
                     result,
                     attempts: ++info.attempts
                 }];
@@ -55,7 +88,7 @@ export default (config: Config) => (obs: Observable) => {
         expand(ii => of(ii)
             .pipe(
                 filter(([, info]) => {
-                    return !info.result.EventId && 
+                    return !info.result.EventId &&
                         RETRY_ON.includes(info.result.ErrorCode) &&
                         info.attempts < config.maxAttempts;
                 }),
