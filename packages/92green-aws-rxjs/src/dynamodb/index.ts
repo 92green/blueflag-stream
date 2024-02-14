5793b4c76de18c8b14ec69618f31360ecba3dd58
Elvey <matt.elvey@blueflag.com.au>
2024-02-15 08:03:57 +0930

    feat: fix exports for queryAll

--- packages/92green-aws-rxjs/src/dynamodb/index.ts
+++ packages/92green-aws-rxjs/src/dynamodb/index.ts
@@ -1,4 +1,4 @@
 export {default as batchGetWithRetry} from './batchGetWithRetry';
 export {default as batchWriteWithRetry} from './batchWriteWithRetry';
-export {default as queryAll} from './queryAll';
+export {queryAll, docClientQueryAll} from './queryAll';
 export {default as scanAll} from './scanAll';
