41bd8a73d4df8085881703e24d520d5e9066e55e
Elvey <matt.elvey@blueflag.com.au>
2024-02-21 11:36:29 +0930

    feat: fix tests broken by changes

--- packages/92green-aws-rxjs/src/dynamodb/__tests__/queryAll.spec.ts
+++ packages/92green-aws-rxjs/src/dynamodb/__tests__/queryAll.spec.ts
@@ -4,7 +4,8 @@ import {mockClient} from "aws-sdk-client-mock";
 import {marshall} from '@aws-sdk/util-dynamodb';
 import {lastValueFrom} from 'rxjs';
 
-import {QueryCommand, DynamoDBClient, QueryCommandOutput} from '@aws-sdk/client-dynamodb';
+import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
+import {DynamoDBDocumentClient, QueryCommand, QueryCommandOutput} from "@aws-sdk/lib-dynamodb";
 
 describe('queryAll', () => {
 
@@ -12,21 +13,25 @@ describe('queryAll', () => {
         let responsePayload: QueryCommandOutput = {
             $metadata:{},
             Items: [
-                marshall({number: 1}), 
-                marshall({number: 2}), 
-                marshall({number: 3})
+                //marshall({number: 1}),
+                //marshall({number: 2}),
+                //marshall({number: 3})
+                {number: 1},
+                {number: 2},
+                {number: 3}
             ],
             Count: 3
         };
         let dynamoDBClient = new DynamoDBClient({});
-        mockClient(dynamoDBClient).on(QueryCommand).resolves(responsePayload)
+        let docClient = DynamoDBDocumentClient.from(dynamoDBClient);
+        mockClient(docClient).on(QueryCommand).resolves(responsePayload)
         let params = {
             TableName: 'fake-table'
         };
 
 
         let response = await lastValueFrom(
-            queryAll(dynamoDBClient, params)
+            queryAll(docClient, params)
                 .pipe(toArray())
         );
 
@@ -59,19 +64,19 @@ describe('queryAll', () => {
                 {
                     $metadata: {},
                     Items: [
-                        marshall(item)
+                        item
                     ],
                     LastEvaluatedKey: marshall({key: "abc"})
                 } as QueryCommandOutput
             ))
             .mockImplementationOnce(() => Promise.resolve({$metadata: {},
                 Items: [
-                    marshall(item2)
+                    item2
                 ]
             } as QueryCommandOutput)
         )
         mockClient(dynamoDBClient).on(QueryCommand).callsFake(fn)
-        
+
 
         let response = await lastValueFrom(
             queryAll(dynamoDBClient, params).pipe(toArray())
@@ -81,51 +86,16 @@ describe('queryAll', () => {
         expect(response).toContainEqual(item2);
     });
 
-    it('should accept feedback pipe that gets used in each feedback loop', async () => {
-        let dynamoDBClient = new DynamoDBClient({});
-        let params = {
-            TableName: 'fake-table'
-        };
-
-        let responsePayloads = [
-            {
-                Items: [100, 200, 300],
-                LastEvaluatedKey: 'foo'
-            },
-            {
-                Items: [400, 500, 600],
-                LastEvaluatedKey: 'bar'
-            },
-            {
-                Items: [700, 800, 900]
-            }
-        ];
-
-        let fn = jest.fn()
-                .mockImplementationOnce(() => Promise.resolve(responsePayloads[0]))
-                .mockImplementationOnce(() => Promise.resolve(responsePayloads[1]))
-                .mockImplementationOnce(() => Promise.resolve(responsePayloads[2]))
-        mockClient(dynamoDBClient).on(QueryCommand).callsFake(fn)
-
-        let feedbackPipeFn = jest.fn(obs => obs);
-
-        await lastValueFrom(
-            queryAll(dynamoDBClient, params, feedbackPipeFn)
-                .pipe(toArray())
-            )
-
-        expect(feedbackPipeFn).toHaveBeenCalledTimes(2);
-
-    });
-
     it('Should handle errors', async () => {
         expect.assertions(1);
 
         let dynamoDBClient = new DynamoDBClient({});
-        mockClient(dynamoDBClient).rejects('!!!')
+        let docClient = DynamoDBDocumentClient.from(dynamoDBClient);
+        mockClient(docClient).rejects('!!!')
         await lastValueFrom(
              queryAll(
-                    dynamoDBClient,
+                    docClient,
+                    // @ts-ignore - test with invalid params
                     {tableName: 'fake-table'}
                 )
             )
--- packages/92green-aws-rxjs/src/dynamodb/queryAll.ts
+++ packages/92green-aws-rxjs/src/dynamodb/queryAll.ts
@@ -1,6 +1,6 @@
 import {
     DynamoDBClient,
-    QueryCommand as DyanmoClientQueryCommand,
+    QueryCommand as DynamoClientQueryCommand,
     QueryCommandInput as DynamoClientQueryCommandInput,
     AttributeValue
 } from '@aws-sdk/client-dynamodb';
@@ -9,12 +9,10 @@ import {
     QueryCommandInput as DocClientQueryCommandInput,
     DynamoDBDocumentClient
 } from "@aws-sdk/lib-dynamodb";
-import {unmarshall} from '@aws-sdk/util-dynamodb';
 import {Observable} from 'rxjs';
 import {from} from 'rxjs';
 import {EMPTY} from 'rxjs';
 import {
-    map,
     concatMap,
     expand
 } from 'rxjs/operators';
@@ -24,7 +22,7 @@ import {
 * By using the document client all marshalling and unmarshalling is handled for you
 * it is worth noting that this is so sufficently simple that it probably makes sense to just write the rxjs logic directly into your workflow instead of using this function
 **/
-queryAll = (docClient: DynamoDBDocumentClient, params: DocClientQueryCommandInput): Observable<Record<string, string>> => {
+export const queryAll = (docClient: DynamoDBDocumentClient, params: DocClientQueryCommandInput): Observable<Record<string, string>> => {
     return from(docClient.send(new DocClientQueryCommand(params))).pipe(
         expand((response) => {
             if(response.LastEvaluatedKey) {
@@ -44,8 +42,8 @@ queryAll = (docClient: DynamoDBDocumentClient, params: DocClientQueryCommandInpu
 * Unlike the default queryAll this takes and returns unmarshalled data and loosely aligns with the previous queryAll function from 92green-aws-rxjs
 * Alike the default version it this is sufficently simple that it probably makes sense to just write the rxjs logic directly into your workflow instead of using this function
 **/
-const queryAllDynamoDBClient = (dynamoClient: DynamoDBClient, params: DynamoClientQueryCommandInput): Observable<{[key: string]: AttributeValue;}> => {
-    return from(dynamoClient.send(new QueryCommand(params))).pipe(
+export const queryAllDynamoDBClient = (dynamoClient: DynamoDBClient, params: DynamoClientQueryCommandInput): Observable<{[key: string]: AttributeValue;}> => {
+    return from(dynamoClient.send(new DynamoClientQueryCommand(params))).pipe(
         expand((response) => {
             if(response.LastEvaluatedKey) {
                 return from(dynamoClient.send(new DynamoClientQueryCommand({
@@ -58,3 +56,5 @@ const queryAllDynamoDBClient = (dynamoClient: DynamoDBClient, params: DynamoClie
         concatMap(response => response.Items ? response.Items : [])
     );
 };
+
+export default queryAll;
